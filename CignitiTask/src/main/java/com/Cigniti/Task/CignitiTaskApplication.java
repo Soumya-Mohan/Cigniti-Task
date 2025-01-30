399d0af9c994f7c912f33c31f039fea9751c3a42
soumyaShee <soumya.sphatige@gmail.com>
2025-01-30 16:49:27 +0530

    Initial commit

---
+++ CignitiTask/src/main/java/com/Cigniti/Task/CignitiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.Cigniti.Task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CignitiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CignitiTaskApplication.class, args);
+	}
+
+}
---
+++ CignitiTask/src/test/java/com/Cigniti/Task/CignitiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cigniti.Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CignitiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
